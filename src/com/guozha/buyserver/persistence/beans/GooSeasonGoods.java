314bd8e1e222989b6cb94c90a07942814337f026
tanxf <359226214@qq.com>
2015-02-08 17:21:20 +0800

    时令养生

--- src/com/guozha/buyserver/persistence/beans/GooSeasonGoods.java
+++ src/com/guozha/buyserver/persistence/beans/GooSeasonGoods.java
@@ -10,6 +10,63 @@ import com.guozha.buyserver.dal.object.AbstractDO;
  * @date 2015-3-11 上午10:53:35
  */
 public class GooSeasonGoods extends AbstractDO {
-	//SEASON_GOODS_ID,SEASON,SEASON_PIC_URL,FIRST_GOODS_ID,SECOND_GOODS_ID,THIRD_GOODS_ID,FOUR_GOODS_ID,FIVE_GOODS_ID
+	private Integer seasonGoodsId;
+	private String season;
+	private String seasonPicUrl;
+	private Integer firstGoodsId;
+	private Integer secondGoodsId;
+	private Integer thirdGoodsId;
+	private Integer fourGoodsId;
+	private Integer fiveGoodsId;
+	
+	
+	public Integer getSeasonGoodsId() {
+		return seasonGoodsId;
+	}
+	public void setSeasonGoodsId(Integer seasonGoodsId) {
+		this.seasonGoodsId = seasonGoodsId;
+	}
+	public String getSeason() {
+		return season;
+	}
+	public void setSeason(String season) {
+		this.season = season;
+	}
+	public String getSeasonPicUrl() {
+		return seasonPicUrl;
+	}
+	public void setSeasonPicUrl(String seasonPicUrl) {
+		this.seasonPicUrl = seasonPicUrl;
+	}
+	public Integer getFirstGoodsId() {
+		return firstGoodsId;
+	}
+	public void setFirstGoodsId(Integer firstGoodsId) {
+		this.firstGoodsId = firstGoodsId;
+	}
+	public Integer getSecondGoodsId() {
+		return secondGoodsId;
+	}
+	public void setSecondGoodsId(Integer secondGoodsId) {
+		this.secondGoodsId = secondGoodsId;
+	}
+	public Integer getThirdGoodsId() {
+		return thirdGoodsId;
+	}
+	public void setThirdGoodsId(Integer thirdGoodsId) {
+		this.thirdGoodsId = thirdGoodsId;
+	}
+	public Integer getFourGoodsId() {
+		return fourGoodsId;
+	}
+	public void setFourGoodsId(Integer fourGoodsId) {
+		this.fourGoodsId = fourGoodsId;
+	}
+	public Integer getFiveGoodsId() {
+		return fiveGoodsId;
+	}
+	public void setFiveGoodsId(Integer fiveGoodsId) {
+		this.fiveGoodsId = fiveGoodsId;
+	}
 
 }
---
+++ src/com/guozha/buyserver/persistence/mapper/GooSeasonGoodsMapper.java
@@ -0,0 +1,18 @@
+package com.guozha.buyserver.persistence.mapper;
+
+import org.springframework.stereotype.Repository;
+
+import com.guozha.buyserver.dal.BaseMapper;
+import com.guozha.buyserver.persistence.beans.GooSeasonGoods;
+
+/**
+ * 节气食材
+ * @Package com.guozha.buyserver.persistence.mapper
+ * @Description: TODO(用一句话描述该文件做什么)
+ * @author txf
+ * @date 2015-3-13 下午4:41:05
+ */
+@Repository
+public interface GooSeasonGoodsMapper extends BaseMapper<GooSeasonGoods, Integer> {
+
+}
