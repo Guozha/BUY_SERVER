57ae77e9180cfc98daa38a055806771a82ba0b05
tanxf <359226214@qq.com>
2015-02-11 13:54:57 +0800

    产品类目 购物车相关

---
+++ src/com/guozha/buyserver/web/controller/cart/ProductTypeResponse.java
@@ -0,0 +1,40 @@
+package com.guozha.buyserver.web.controller.cart;
+
+import java.util.List;
+
+/**
+ * 产品类别
+ * @Package com.guozha.buyserver.web.controller.cart
+ * @Description: TODO(用一句话描述该文件做什么)
+ * @author txf
+ * @date 2015-2-11 上午9:47:31
+ */
+public class ProductTypeResponse {
+	
+	public ProductTypeResponse(String productType) {
+		super();
+		this.productType = productType;
+	}
+
+	private String productType;
+	private List<CartResponse> cartList;
+	
+
+	public List<CartResponse> getCartList() {
+		return cartList;
+	}
+
+	public void setCartList(List<CartResponse> cartList) {
+		this.cartList = cartList;
+	}
+
+	public String getProductType() {
+		return productType;
+	}
+
+	public void setProductType(String productType) {
+		this.productType = productType;
+	}
+	
+
+}
