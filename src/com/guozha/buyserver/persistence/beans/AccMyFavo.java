d1722655d37d02bc5d0b6efc8e178ef4797d6d4b
zhutx <16530202@qq.com>
2015-03-13 17:18:52 +0800

    创建我的收藏模块的po类

--- src/com/guozha/buyserver/persistence/beans/AccMyFavo.java
+++ src/com/guozha/buyserver/persistence/beans/AccMyFavo.java
@@ -3,5 +3,35 @@ package com.guozha.buyserver.persistence.beans;
 import com.guozha.buyserver.dal.object.AbstractDO;
 
 public class AccMyFavo extends AbstractDO {
+	
+	private Integer myFavoId;
+	private Integer userId;
+	private String favoName;
+	private Integer sort;
+	
+	public Integer getMyFavoId() {
+		return myFavoId;
+	}
+	public void setMyFavoId(Integer myFavoId) {
+		this.myFavoId = myFavoId;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getFavoName() {
+		return favoName;
+	}
+	public void setFavoName(String favoName) {
+		this.favoName = favoName;
+	}
+	public Integer getSort() {
+		return sort;
+	}
+	public void setSort(Integer sort) {
+		this.sort = sort;
+	}
 
 }
--- src/com/guozha/buyserver/persistence/beans/AccMyFavoDetail.java
+++ src/com/guozha/buyserver/persistence/beans/AccMyFavoDetail.java
@@ -3,5 +3,35 @@ package com.guozha.buyserver.persistence.beans;
 import com.guozha.buyserver.dal.object.AbstractDO;
 
 public class AccMyFavoDetail extends AbstractDO {
+	
+	private Integer myFavoDetailId;
+	private Integer myFavoId;
+	private Integer menuId;
+	private Integer sort;
+	
+	public Integer getMyFavoDetailId() {
+		return myFavoDetailId;
+	}
+	public void setMyFavoDetailId(Integer myFavoDetailId) {
+		this.myFavoDetailId = myFavoDetailId;
+	}
+	public Integer getMyFavoId() {
+		return myFavoId;
+	}
+	public void setMyFavoId(Integer myFavoId) {
+		this.myFavoId = myFavoId;
+	}
+	public Integer getMenuId() {
+		return menuId;
+	}
+	public void setMenuId(Integer menuId) {
+		this.menuId = menuId;
+	}
+	public Integer getSort() {
+		return sort;
+	}
+	public void setSort(Integer sort) {
+		this.sort = sort;
+	}
 
 }
