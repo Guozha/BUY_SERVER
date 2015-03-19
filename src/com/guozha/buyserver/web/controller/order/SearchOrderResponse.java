b101182eba2b921abad9cec9cf5ea00611eebc7b
zhutx <16530202@qq.com>
2015-03-19 14:31:54 +0800

    完成订单查询

---
+++ src/com/guozha/buyserver/web/controller/order/SearchOrderResponse.java
@@ -0,0 +1,51 @@
+package com.guozha.buyserver.web.controller.order;
+
+import java.util.Date;
+
+public class SearchOrderResponse {
+	
+	private Integer orderId;
+	private String orderNo;
+	private Date createTime;
+	private int quantity;
+	private int totalPrice;
+	private String status;
+	
+	public Integer getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(Integer orderId) {
+		this.orderId = orderId;
+	}
+	public String getOrderNo() {
+		return orderNo;
+	}
+	public void setOrderNo(String orderNo) {
+		this.orderNo = orderNo;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
