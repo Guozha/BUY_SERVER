377a667c725459f90fa2196be8a3494d4b1be640
sunailian <sunangie@126.com>
2015-03-25 17:45:28 +0800

    短信管理功能

---
+++ src/com/guozha/buyserver/persistence/mapper/SysSmsSendMapper.java
@@ -0,0 +1,19 @@
+package com.guozha.buyserver.persistence.mapper;
+
+import org.springframework.stereotype.Repository;
+
+import com.guozha.buyserver.dal.BaseMapper;
+import com.guozha.buyserver.persistence.beans.SysSmsSend;
+
+/**
+ * @Package com.guozha.buyserver.persistence.mapper
+ * @Description: 发送短信
+ * @author sunhanbin
+ * @date 2015-3-25 下午05:17:55
+ */
+@Repository
+public interface SysSmsSendMapper extends BaseMapper<SysSmsSend, Integer>{
+	
+	
+
+}
